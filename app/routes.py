86e426490f6f4c21267b7858ee8190eae8f7d5bb
Elliott Yu <elliottyu@Elliotts-MacBook-Pro.local>
2019-11-16 19:13:46 +0000

    deployed

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
 from flask import render_template,flash,redirect
 from app import app
-from app.forms import LoginForm
 
 
 @app.route('/')
@@ -8,13 +7,3 @@ from app.forms import LoginForm
 def index():
     user = {'username': 'OxfordHack'}
     return render_template('index.html', title='Home', user=user)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
