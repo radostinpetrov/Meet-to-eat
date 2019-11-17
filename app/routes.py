30d7a9df8aaf0552749571d209c31c31cbb90dee
Elliott Yu <elliottyu@Elliotts-MacBook-Pro.local>
2019-11-17 05:42:02 +0000

    deployed

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,25 @@
 from flask import render_template, flash, redirect, request, url_for
-from app import app, db
+from app import app
+from app import db
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
 from app.forms import RegistrationForm
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
+@app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    return render_template('index.html', title='Home')
+    #print(user.username)
+    return render_template('index.html', title = 'home')
 
-@app.route('/')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -23,10 +31,9 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
+        #if not next_page or url_parse(next_page).netloc != '':
+        #   next_page = url_for('index')
+        return redirect('user/' + user.username)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
@@ -48,4 +55,11 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+#@app.route('/query', methods=["GET"])
+#def dump():
+#    dump = ""
+#    for user in User.query.filter_by():
+#        dump += user.username + " "
+#    return dump
+    
 
