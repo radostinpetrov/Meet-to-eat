854dad4750fb45fdabd4319daa133f124a82052b
jy8518 <john.yao18@imperial.ac.uk>
2019-11-17 00:25:00 +0000

    added password generation back

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 from app import login
 from datetime import datetime
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @login.user_loader
 def load_user(id):
